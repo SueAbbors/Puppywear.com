1d527018539bb96de25d6bce683339bf9e3c2d8a
Sue M. MacDonald <smacdonald@hydro.mb.ca>
2015-04-01 12:53:31 -0500

    deleted image_filename and images installed carrierwave and mini_magick and reinstalled images

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Product do
   
- permit_params :title, :description, :price, :image_filename, :stock_quantity
+ permit_params :title, :description, :price, :image, :stock_quantity
 
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
-  validates :title, :price, :stock_quantity, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :price, :stock_quantity, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :stock_quantity, numericality: {only_integer: true}
+  
+  mount_uploader :image, ProductImageUploader
 end
