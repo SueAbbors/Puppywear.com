4eecbac618eac37c6aaf3ade161ea14941698963
Sue M. MacDonald <smacdonald@hydro.mb.ca>
2015-04-05 11:15:44 -0500

    Added categories to the front end

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,11 @@
 class CategoriesController < ApplicationController
 
   def index
-    @category = Category.all
+    @categories = Category.all
   end
  
   def show
+    @category = Category.find(params[:id])
   end
   
-  def new
-    @category = Category.new
-  end
 end
