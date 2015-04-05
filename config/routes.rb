04c40b0ecf1521265c217c9a0c1eb9ad149eae73
Sue M. MacDonald <smacdonald@hydro.mb.ca>
2015-04-05 13:26:08 -0500

    Added my search for keywords or letters for my project

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
  root 'products#index'
   
- #get    'products',           to: 'products#index',     as:  'products'
- #get    'products/new',       to: 'products#new',       as:  'new_product'
- get     'products/:id',       to: 'products#show',      as:  'product'
+ #get    'products',           to: 'products#index',            as:  'products'
+ #get    'products/new',       to: 'products#new',              as:  'new_product'
+ get     'products/:id',       to: 'products#show',             as:  'product'
  #post   'products',           to: 'products#create'
- #get    'products/:id/edit',  to: 'products#edit',      as:  'edit_product'
+ #get    'products/:id/edit',  to: 'products#edit',             as:  'edit_product'
  #patch  'products/:id',       to: 'products#update'
- #delete 'products/:id',       to: 'products#destroy',   as:  'delete_product'
- resources :categories
+ #delete 'products/:id',       to: 'products#destroy',          as:  'delete_product'
+ root to: 'categories#index'
+ get      'search_results',    to: 'products#search_results',   as: 'search_results'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
