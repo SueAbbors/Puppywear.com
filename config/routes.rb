6759dc6b62c5d63f49cc05e38fbb3e173515934f
Sue M. MacDonald <smacdonald@hydro.mb.ca>
2015-04-07 21:26:21 -0500

    created about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'pages/about'
+
+  get 'pages/contact'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
  root 'products#index'
